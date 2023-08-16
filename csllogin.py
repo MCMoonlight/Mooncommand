d463264a7e18219096873fd9e61da23c18217e22
Administrator <Administrator@PC-20211214JQHE>
2023-08-16 15:12:27 +0800

    update login

--- csllogin.py
+++ csllogin.py
@@ -1,5 +1,11 @@
 #模块
+import wget
+
 def login():
+    url2 = "https://gitee.com/bilibiliFSRH/cslcommand/raw/main/cslusername.py"
+    url1 = "https://gitee.com/bilibiliFSRH/cslcommand/raw/main/csluserpass.py"
+    wget.download(url2)
+    wget.download(url1)
     userfile = open("cslusername.txt", "r")
     try:
         username = userfile.read()
@@ -15,6 +21,6 @@ def login():
     password1 = input("Enter csl account password(no csl account please enter NO)")
     username1 = input("Enter csl account username(no csl account please enter NO)")
     if password1 == password or username1 == username:
-        print("OK")
-    else:
         print("Welcome csl users")
+    else:
+        print("OK")
