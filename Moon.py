a4a71c4ffa0487b4238c45123138cfe01db9c187
FSRH <3565723760@QQ.com>
2023-12-24 21:20:44 +0800

    草泥马

--- Moon.py
+++ Moon.py
@@ -1,8 +1,8 @@
-file = input("请输入你的文件:")
+file = input("请输入你的文件:") #输入要运行的文件
 
-def aout(a,b):
-    return a + b
-with open(file) as rf:
+def aout(a,b): ##定义aout模块，包括定义a,b
+    return a + b ## 返回a+b
+with open(file) as rf: 
     content = rf.read()
     if "main:" in content:
         content = content.replace("main:", "")
